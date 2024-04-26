bb72a87f52453fe31588fb75386707b29d856243
poula1010 <85223285+poula1010@users.noreply.github.com>
2024-04-26 14:01:30 +0300

    Update IP_PacketData.h
    changed data type of source and destianation ip from array to vector to support ipv6 and ipv4

--- include/IP_PacketData.h
+++ include/IP_PacketData.h
@@ -1,8 +1,8 @@
+#pragma once
 #include <array>
 #include <vector>
 #include <string>
 
-#pragma once
 using std::array;
 using std::string;
 using std::vector;
@@ -19,8 +19,8 @@ public:
     unsigned char timeToLive;
     unsigned char protocol;
     array<unsigned char, 2> headerCheckSum;
-    array<unsigned char, 4> sourceIp;
-    array<unsigned char, 4> destinationIp;
+    vector<unsigned char> sourceIp;
+    vector<unsigned char> destinationIp;
     vector<unsigned char> options;
     vector<unsigned char> ipData;
 
@@ -34,8 +34,8 @@ public:
                   unsigned char timeToLive,
                   unsigned char protocol,
                   array<unsigned char, 2> headerCheckSum,
-                  array<unsigned char, 4> sourceIp,
-                  array<unsigned char, 4> destinationIp,
+                  vector<unsigned char> sourceIp,
+                  vector<unsigned char> destinationIp,
                   vector<unsigned char> options,
                   vector<unsigned char> ipData) : ipVersion{ipVersion},
                                                   internetHeaderLength{internetHeaderLength},
